e6b046ea6e5d9697622a520742f97a183b7fda09
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2025-12-31 17:58:41 +0000

    w

--- 2_platform_connectivity/environments/dev-gwc/outputs.tf
+++ 2_platform_connectivity/environments/dev-gwc/outputs.tf
@@ -1,105 +1,105 @@
-# -----------------------------------------------------------------------------
-# Outputs - Germany West Central Hub
-# Used by global environment and landing zones via remote state
-# -----------------------------------------------------------------------------
+# # -----------------------------------------------------------------------------
+# # Outputs - Germany West Central Hub
+# # Used by global environment and landing zones via remote state
+# # -----------------------------------------------------------------------------
 
-# Hub VNet
-output "hub_vnet_id" {
-  description = "ID of the hub virtual network"
-  value       = module.hub_vnet.id
-}
+# # Hub VNet
+# output "hub_vnet_id" {
+#   description = "ID of the hub virtual network"
+#   value       = module.hub_vnet.id
+# }
 
-output "hub_vnet_name" {
-  description = "Name of the hub virtual network"
-  value       = module.hub_vnet.name
-}
+# output "hub_vnet_name" {
+#   description = "Name of the hub virtual network"
+#   value       = module.hub_vnet.name
+# }
 
-output "hub_resource_group_name" {
-  description = "Name of the hub resource group"
-  value       = module.hub_rg.name
-}
+# output "hub_resource_group_name" {
+#   description = "Name of the hub resource group"
+#   value       = module.hub_rg.name
+# }
 
-output "hub_location" {
-  description = "Location of the hub"
-  value       = var.location
-}
+# output "hub_location" {
+#   description = "Location of the hub"
+#   value       = var.location
+# }
 
-# NVA
-output "nva_lb_ip" {
-  description = "Load balancer IP for NVA (use as next hop)"
-  value       = module.nva_ha.lb_private_ip
-}
+# # NVA
+# output "nva_lb_ip" {
+#   description = "Load balancer IP for NVA (use as next hop)"
+#   value       = module.nva_ha.lb_private_ip
+# }
 
-output "nva_primary_ip" {
-  description = "Primary NVA private IP (for Ansible)"
-  value       = module.nva_ha.nva_primary_private_ip
-}
+# output "nva_primary_ip" {
+#   description = "Primary NVA private IP (for Ansible)"
+#   value       = module.nva_ha.nva_primary_private_ip
+# }
 
-output "nva_secondary_ip" {
-  description = "Secondary NVA private IP (for Ansible)"
-  value       = module.nva_ha.nva_secondary_private_ip
-}
+# output "nva_secondary_ip" {
+#   description = "Secondary NVA private IP (for Ansible)"
+#   value       = module.nva_ha.nva_secondary_private_ip
+# }
 
-output "nva_primary_vm_name" {
-  description = "Primary NVA VM name (for Ansible)"
-  value       = module.nva_ha.nva_primary_vm_name
-}
+# output "nva_primary_vm_name" {
+#   description = "Primary NVA VM name (for Ansible)"
+#   value       = module.nva_ha.nva_primary_vm_name
+# }
 
-output "nva_secondary_vm_name" {
-  description = "Secondary NVA VM name (for Ansible)"
-  value       = module.nva_ha.nva_secondary_vm_name
-}
+# output "nva_secondary_vm_name" {
+#   description = "Secondary NVA VM name (for Ansible)"
+#   value       = module.nva_ha.nva_secondary_vm_name
+# }
 
-# DNS Zones
-output "dns_zone_blob_id" {
-  description = "ID of the blob private DNS zone"
-  value       = module.dns_zone_blob.id
-}
+# # DNS Zones
+# output "dns_zone_blob_id" {
+#   description = "ID of the blob private DNS zone"
+#   value       = module.dns_zone_blob.id
+# }
 
-output "dns_zone_blob_name" {
-  description = "Name of the blob private DNS zone"
-  value       = module.dns_zone_blob.name
-}
+# output "dns_zone_blob_name" {
+#   description = "Name of the blob private DNS zone"
+#   value       = module.dns_zone_blob.name
+# }
 
-output "dns_zone_keyvault_id" {
-  description = "ID of the keyvault private DNS zone"
-  value       = module.dns_zone_keyvault.id
-}
+# output "dns_zone_keyvault_id" {
+#   description = "ID of the keyvault private DNS zone"
+#   value       = module.dns_zone_keyvault.id
+# }
 
-output "dns_zone_keyvault_name" {
-  description = "Name of the keyvault private DNS zone"
-  value       = module.dns_zone_keyvault.name
-}
+# output "dns_zone_keyvault_name" {
+#   description = "Name of the keyvault private DNS zone"
+#   value       = module.dns_zone_keyvault.name
+# }
 
-# Jumpbox
-output "jumpbox_private_ip" {
-  description = "Private IP of the jumpbox"
-  value       = module.jumpbox.private_ip
-}
+# # Jumpbox
+# output "jumpbox_private_ip" {
+#   description = "Private IP of the jumpbox"
+#   value       = module.jumpbox.private_ip
+# }
 
-# Key Vault
-output "key_vault_name" {
-  description = "Name of the Key Vault containing VM SSH keys"
-  value       = module.key_vault.name
-}
+# # Key Vault
+# output "key_vault_name" {
+#   description = "Name of the Key Vault containing VM SSH keys"
+#   value       = module.key_vault.name
+# }
 
-output "key_vault_uri" {
-  description = "URI of the Key Vault"
-  value       = module.key_vault.vault_uri
-}
+# output "key_vault_uri" {
+#   description = "URI of the Key Vault"
+#   value       = module.key_vault.vault_uri
+# }
 
-output "vm_ssh_key_vault_secret_name" {
-  description = "Name of the Key Vault secret containing the VM SSH private key"
-  value       = azurerm_key_vault_secret.vm_ssh_private_key.name
-}
+# output "vm_ssh_key_vault_secret_name" {
+#   description = "Name of the Key Vault secret containing the VM SSH private key"
+#   value       = azurerm_key_vault_secret.vm_ssh_private_key.name
+# }
 
-# Platform Spokes
-output "identity_vnet_id" {
-  description = "ID of the identity spoke VNet"
-  value       = module.identity_vnet.id
-}
+# # Platform Spokes
+# output "identity_vnet_id" {
+#   description = "ID of the identity spoke VNet"
+#   value       = module.identity_vnet.id
+# }
 
-output "management_vnet_id" {
-  description = "ID of the management spoke VNet"
-  value       = module.management_vnet.id
-}
+# output "management_vnet_id" {
+#   description = "ID of the management spoke VNet"
+#   value       = module.management_vnet.id
+# }
