012efae10ef2915452f7adc39553f3fdfceafb2d
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2026-01-04 15:31:13 +0000

    w

--- 1_platform_management/environments/dev/backend.tf
+++ 1_platform_management/environments/dev/backend.tf
@@ -3,5 +3,6 @@ terraform {
     resource_group_name  = "rg-tfstate-co-dev-gwc-01"
     storage_account_name = "sttfstatecodevgwc01"
     container_name       = "tfstate-pl-management"
+    subscription_id      = "e388ddce-c79d-4db0-8a6f-cd69b1708954"
   }
 }
