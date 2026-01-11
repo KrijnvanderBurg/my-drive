04d427f4080b854af04f7b77412994012698a855
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2026-01-11 09:19:55 +0000

    W

--- 3_alz_drives/environments/dev/backend.tf
+++ 3_alz_drives/environments/dev/backend.tf
@@ -4,5 +4,6 @@ terraform {
     resource_group_name  = "rg-tfstate-co-dev-gwc-01"
     storage_account_name = "sttfstatecodevgwc01"
     container_name       = "tfstate-alz-drive"
+    use_azuread_auth     = true
   }
 }
