f5ed652e9a1735fdb140621d53393e961c0250e9
Krijn van der Burg <krijn.vander.burg@tennet.eu>
2026-01-19 09:40:00 +0100

    w

--- 03-platform-connectivity/environments/dev-glb/outputs.tf
+++ 03-platform-connectivity/environments/dev-glb/outputs.tf
@@ -6,14 +6,14 @@ output "hub_peerings" {
   description = "Hub-to-hub peering details"
   value = {
     weu_to_gwc = {
-      id                  = module.hub_weu_to_hub_gwc.id
-      name                = module.hub_weu_to_hub_gwc.name
-      peering_state       = module.hub_weu_to_hub_gwc.peering_state
+      id            = module.hub_weu_to_hub_gwc.id
+      name          = module.hub_weu_to_hub_gwc.name
+      peering_state = module.hub_weu_to_hub_gwc.peering_state
     }
     gwc_to_weu = {
-      id                  = module.hub_gwc_to_hub_weu.id
-      name                = module.hub_gwc_to_hub_weu.name
-      peering_state       = module.hub_gwc_to_hub_weu.peering_state
+      id            = module.hub_gwc_to_hub_weu.id
+      name          = module.hub_gwc_to_hub_weu.name
+      peering_state = module.hub_gwc_to_hub_weu.peering_state
     }
   }
 }
--- 03-platform-connectivity/environments/dev-weu/providers.tf
+++ 03-platform-connectivity/environments/dev-weu/providers.tf
@@ -17,18 +17,31 @@ provider "azurerm" {
 }
 
 # =============================================================================
-# Remote State Data Sources
+# Remote State - Management Layer
 # =============================================================================
 
 data "terraform_remote_state" "management" {
   backend = "azurerm"
 
   config = {
-    subscription_id      = "e388ddce-c79d-4db0-8a6f-cd69b1708954"
-    resource_group_name  = "rg-tfstate-co-dev-gwc-01"
-    storage_account_name = "sttfstatecodevgwc01"
-    container_name       = "tfstate-pl-management"
-    key                  = "pl-management-dev.tfstate"
-    use_azuread_auth     = true
+    resource_group_name  = "rg-terraform-state-on-dev-glb-01"
+    storage_account_name = "sttfstateondevglb01"
+    container_name       = "tfstate"
+    key                  = "02-platform-management/dev-glb.tfstate"
+  }
+}
+
+# =============================================================================
+# Remote State - GWC Region
+# =============================================================================
+
+data "terraform_remote_state" "gwc" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = "rg-terraform-state-on-dev-glb-01"
+    storage_account_name = "sttfstateondevglb01"
+    container_name       = "tfstate"
+    key                  = "03-platform-connectivity/dev-gwc.tfstate"
   }
 }
