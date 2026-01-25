c7d5b9f2c28fff51dfa74d482888d9e8b4d27fab
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2026-01-25 15:04:38 +0000

    w

--- 02-platform-identity/modules/06-entra-group/main.tf
+++ 02-platform-identity/modules/06-entra-group/main.tf
@@ -1,7 +1,6 @@
 resource "azuread_group" "this" {
-  display_name            = var.display_name
-  description             = var.description
-  security_enabled        = true
-  assignable_to_role      = var.assignable_to_role
-  prevent_duplicate_names = true
+  display_name       = var.display_name
+  description        = var.description
+  security_enabled   = true
+  assignable_to_role = var.assignable_to_role
 }
