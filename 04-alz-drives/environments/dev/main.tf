e20ca9332a2bc1ea8b6a8c03f5122317f54cb11e
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2026-01-25 18:15:28 +0000

    w

--- 04-alz-drives/environments/dev/main.tf
+++ 04-alz-drives/environments/dev/main.tf
@@ -2,16 +2,16 @@
 # Dev Environment - Platform Storage
 # =============================================================================
 
-module "drive" {
-  source = "../../modules/drive"
+# module "drive" {
+#   source = "../../modules/drive"
 
-  environment    = local.environment
-  location_short = local.location_short
-  location       = local.location
-  allowed_ips    = local.allowed_ips
-  tags           = local.common_tags
+#   environment    = local.environment
+#   location_short = local.location_short
+#   location       = local.location
+#   allowed_ips    = local.allowed_ips
+#   tags           = local.common_tags
 
-  containers = [
-    "tier1",
-  ]
-}
+#   containers = [
+#     "tier1",
+#   ]
+# }
