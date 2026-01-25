d85066f9ab67e8f8aff1a8498be99954e5cfff16
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2026-01-25 18:18:02 +0000

    w

--- 04-alz-drives/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    subscription_id      = "e388ddce-c79d-4db0-8a6f-cd69b1708954"
-    resource_group_name  = "rg-tfstate-co-dev-gwc-01"
-    storage_account_name = "sttfstatecodevgwc01"
-    container_name       = "tfstate-alz-drive"
-    use_azuread_auth     = true
-  }
-}
